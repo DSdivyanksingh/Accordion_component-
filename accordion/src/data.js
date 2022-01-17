e5f353bdb00398db24925ae9de1345a8955e83ed
divyank12singh <divyanksingh1297@gmail.com>
2022-01-17 20:14:17 +0530

    Multi_Open_Accordion

---
+++ accordion/src/data.js
@@ -0,0 +1,15 @@
+const data=[
+    {
+        heading:"item1",
+        content:"A keyboard accessible before & after component for React",
+    },
+    {
+        heading:"item2",
+        content:"A keyboard accessible before & after component for React",
+    },
+    {
+        heading:"item3",
+        content:"A keyboard accessible before & after component for React",
+    }
+];
+export default data;
